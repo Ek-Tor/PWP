61d53d1ff9ea07717a5a6a06c621f5a99f58dfb4
Aripez A. Hector <hear8429@gmail.com>
2025-06-19 08:45:37 +0000

    menu buttons done.

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
-const list = document.querySelectorAll('.list');
-                function activelink()
+const list = document.querySelectorAll(".list");
+                function activeLink()
                 {
                     list.forEach((item)=>
-                    item.classList.remove('active'));
-                    this.classList.add('active');
+                    item.classList.remove("active"));
+                    this.classList.add("active");
                 }
                 list.forEach((item) =>
-                item.addEventListener('click',activelink));
+                item.addEventListener("click",activeLink));
 
 
 
-// function update(){
-// //this gets the user input from the text box and stores that string into a variable
-// let input = document.getElementById("name").value;
+function update(){
+//this gets the user input from the text box and stores that string into a variable
+let input = document.getElementById("name").value;
 
-// //this will be changing the elements that have the tag name
-// // with Welcome: + the users input from the text box
-// document.getElementById("bread").innerHTML = "Hello " + input ;
-// };
+//this will be changing the elements that have the tag name
+// with Welcome: + the users input from the text box
+document.getElementById("bread").innerHTML = "Hello " + input ;
+};
 
-//  //pop-up alert
-// $( document ).ready(function() {
-//     $('#btn').click(function() {
-//         window.alert("DID YOU JUST CLICK ME?!");
-//     });
-// });
+ //pop-up alert
+$( document ).ready(function() {
+    $('#btn').click(function() {
+        window.alert("DID YOU JUST CLICK ME?!");
+    });
+});
 
