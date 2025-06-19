313a06b8830fcf8fa3f25806dbefef39d7dfc170
Aripez A. Hector <hear8429@gmail.com>
2025-06-19 07:29:26 +0000

    . ,]#	deleted:    node_modules/@ionic/react/node_modules/ionicons/dist/svg/calendar-number-outline.svg

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
-function update(){
-//this gets the user input from the text box and stores that string into a variable
-let input = document.getElementById("name").value;
+const list = document.querySelectorAll('.list');
+                function activelink()
+                {
+                    list.forEach((item)=>
+                    item.classList.remove('active'));
+                    this.classList.add('active');
+                }
+                list.forEach((item) =>
+                item.addEventListener('click',activelink));
 
-//this will be changing the elements that have the tag name
-// with Welcome: + the users input from the text box
-document.getElementById("bread").innerHTML = "Hello " + input ;
-};
 
- //pop-up alert
-$( document ).ready(function() {
-    $('#btn').click(function() {
-        window.alert("DID YOU JUST CLICK ME?!");
-    });
-});
+
+// function update(){
+// //this gets the user input from the text box and stores that string into a variable
+// let input = document.getElementById("name").value;
+
+// //this will be changing the elements that have the tag name
+// // with Welcome: + the users input from the text box
+// document.getElementById("bread").innerHTML = "Hello " + input ;
+// };
+
+//  //pop-up alert
+// $( document ).ready(function() {
+//     $('#btn').click(function() {
+//         window.alert("DID YOU JUST CLICK ME?!");
+//     });
+// });
+
